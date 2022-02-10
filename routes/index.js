d3a397f09a98c51bc7a717c2bf799ae237dae597
aircode91 <36924917+aircode91@users.noreply.github.com>
2022-02-10 18:16:46 +0700

    fix route

--- routes/index.js
+++ routes/index.js
@@ -24,29 +24,8 @@ router.get('/api', function (req, res) {
 router.post('/api/users', Register);
 router.post('/api/login', Login);
 router.get('/api/token', refreshToken);
-router.post('/api/expresence', getDataExpresence);
-router.post('/api/checkInOut', CheckInOut);
+router.post('/api/expresence', getDataExpresence, verifyToken);
+router.post('/api/checkInOut', CheckInOut, verifyToken);
 
-// Profile & Password
-// router.get('/api/me',verifyToken,permissionCheck(['view-profile']), Me);
-// router.post('/api/update-me',verifyToken,permissionCheck(['update-profile']), UpdateMe);
-// router.post('/api/change-password',verifyToken,permissionCheck(['change-password']), ChangePassword);
-// router.post('/api/reset-password',verifyToken,permissionCheck(['reset-password']), ResetPassword);
-
-// CRUD User
-// router.get('/api/users', verifyToken, permissionCheck(['view-list-users']),getUsers);
-// router.post('/api/user/create',verifyToken,permissionCheck(['create-user']),Create);
-// router.get('/api/user/:id',verifyToken,permissionCheck(['view-user']),Show);
-// router.put('/api/user/:id',verifyToken,permissionCheck(['update-user']),Update);
-// router.delete('/api/user/:id',verifyToken,permissionCheck(['delete-user']),Delete);
-
-// route roles 
-// router.get('/api/roles',getRoles);
-// router.post('/api/roles',createRole);
-
-// router.post('/api/permission',verifyToken,getPermission);
-// route routes 
-// router.get('/api/routes',verifyToken,getRoutes);
-// router.post('/api/routes',verifyToken,createRoute);
 
 export default router;
